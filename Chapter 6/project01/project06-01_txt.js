66c936e5470b2fccea980ba0b80ad77e84b2d5ac
Josephg17 <gjelajjoseph313@gmail.com>
2024-07-21 13:25:38 -0400

    project 1 and 2 done

--- Chapter 6/project01/project06-01_txt.js
+++ Chapter 6/project01/project06-01_txt.js
@@ -4,9 +4,28 @@
       Project 06-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Joseph Gjelaj
+      Date:   7/21/24
 
       Filename: project06-01.js
 */
+let submitButton = document.getElementById('submitButton');
+let pwd = document.getElementById('pwd');
+let pwd2 = document.getElementById('pwd2');
 
+
+submitButton.addEventListener('click', function(event) {
+    
+      if (!pwd.value.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+          
+          alert("Your password must be at least 8 characters with at least one letter and one number");
+          event.preventDefault(); 
+      } else if (pwd.value !== pwd2.value) {
+          // Passwords do not match
+          alert("Your passwords must match");
+          event.preventDefault(); 
+      } else {
+
+      }
+  });
+  
--- Chapter 6/project02/project06-02_txt.js
+++ Chapter 6/project02/project06-02_txt.js
@@ -4,10 +4,36 @@
       Project 06-02
 
       Project to turn a selection list into a selection of hypertext links
-      Author: 
-      Date:   
+      Author: Joseph Gjelaj
+      Date:   7-21-24
 
       Filename: project06-02.js
 */
 
+document.addEventListener('DOMContentLoaded', function() {
 
+  });
+  
+
+document.addEventListener('DOMContentLoaded', function() {
+
+      let allSelect = document.querySelectorAll('form#govLinks select');
+      
+
+      allSelect.forEach(function(select) {
+
+          select.addEventListener('change', function(evt) {
+
+              let linkURL = evt.target.value;
+              
+
+              let newWin = window.open(linkURL);
+              
+
+              if (newWin) {
+                  newWin.focus();
+              }
+          });
+      });
+  });
+  
